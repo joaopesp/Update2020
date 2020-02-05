3d642639f42bf18d4b83c2d2f1223883ee39b7bb
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-05 00:59:22 -0300

    Aula231

---
+++ Aula223/Data/Aula223Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula223.Models
+{
+    public class Aula223Context : DbContext
+    {
+        public Aula223Context (DbContextOptions<Aula223Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula223.Models.Department> Department { get; set; }
+    }
+}
--- Aula223/Startup.cs
+++ Aula223/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Aula223.Models;
 
 namespace Aula223
 {
@@ -33,6 +35,9 @@ namespace Aula223
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Aula223Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Aula223Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
