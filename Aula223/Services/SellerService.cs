015c16350e1aae8d101c7e0224a1d973425f4e74
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-06 02:23:25 -0300

    Aula240

--- Aula223/Services/SellerService.cs
+++ Aula223/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace Aula223.Services
 
         public void Inset(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
