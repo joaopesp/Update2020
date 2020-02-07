99790d0b5f0ef8965061f92823983a6704abd7f4
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-06 21:53:05 -0300

    Aula242

--- Aula223/Services/SellerService.cs
+++ Aula223/Services/SellerService.cs
@@ -27,5 +27,17 @@ namespace Aula223.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
