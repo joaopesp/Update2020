bc2b6ee2169dc622e1a4d2bb28f023edf05bf762
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-07 00:20:05 -0300

    Aula246

--- Aula223/Models/Seller.cs
+++ Aula223/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace Aula223.Models
@@ -8,8 +9,19 @@ namespace Aula223.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "BaseSalary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
