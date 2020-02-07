6b3d126e6429e18976dfed00cf1ac94185bb9ca5
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-07 01:28:05 -0300

    Aula247

--- Aula223/Controllers/SellersController.cs
+++ Aula223/Controllers/SellersController.cs
@@ -40,6 +40,12 @@ namespace Aula223.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Inset(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -106,6 +112,13 @@ namespace Aula223.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
