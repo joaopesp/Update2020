f95d5afd99a3fd286387d2ff5316db67b2ef48af
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-06 23:31:26 -0300

    Aula244

--- Aula223/Services/SellerService.cs
+++ Aula223/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Aula223.Models;
 using Microsoft.EntityFrameworkCore;
+using Aula223.Services.Exceptions;
 
 namespace Aula223.Services
 {
@@ -38,6 +39,24 @@ namespace Aula223.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
         }
 
         internal void Remove(int id)
