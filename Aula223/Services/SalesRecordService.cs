80b87b51224cec38a51b1c55980c6eda87e19c5a
João Pedro Espinosa <joao.pedro.espinosa.96@gmail.com>
2020-02-07 17:01:13 -0300

    Aula251

---
+++ Aula223/Services/SalesRecordService.cs
@@ -0,0 +1,39 @@
+﻿using Aula223.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula223.Services
+{
+    public class SalesRecordService
+    {
+
+        private readonly Aula223Context _context;
+
+        public SalesRecordService(Aula223Context context)
+        {
+            _context = context;
+        }
+
+        public async Task <List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= minDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
